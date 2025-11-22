c7683f2d2dac086ce2794b171d6211441ce63e4c
Willy <willy_1020@naver.com>
2025-11-22 13:00:36 +0900

    File(#1): 프로젝트 기본 세팅

---
+++ src/main/java/com/sopt/sopkathon/SopkathonApplication.java
@@ -0,0 +1,13 @@
+package com.sopt.sopkathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopkathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SopkathonApplication.class, args);
+	}
+
+}
